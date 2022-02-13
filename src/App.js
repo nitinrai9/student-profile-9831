7cd6c659390c01e48f53b677f96626a10258e044
nitin <nitin.ry9@gmail.com>
2022-02-13 15:03:41 -0500

    added listview

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import React, { useEffect, useState } from "react";
 import './App.css';
+
+
 class MyComponent extends React.Component {
   constructor(props) {
     super(props);
@@ -18,6 +20,8 @@ class MyComponent extends React.Component {
     this.setState({value: event.target.value});
   
   }
+
+ 
 
 
   componentDidMount() {
@@ -44,8 +48,9 @@ class MyComponent extends React.Component {
 
   render() {
     const { error, isLoaded, items,value} = this.state;
+    var i = 0;
     const itemArray = items.students;
-   
+    var rowCount=0;
 
     if (error) {
       return <div>Error: {error.message}</div>;
@@ -53,7 +58,7 @@ class MyComponent extends React.Component {
       return <div>Loading...</div>;
     } else if(this.state.value!='') {
       return (
- 
+        
         <div>
 
             <div class="row textbox-pad">
@@ -96,7 +101,7 @@ class MyComponent extends React.Component {
     }
     else{
       return (
-       
+          
         <div>
 
             <div class="row textbox-pad">
@@ -113,18 +118,35 @@ class MyComponent extends React.Component {
               itemArray.map((item) => (
             
             <div class="row add-pad">
-              {console.log(value)}
+              
               <div class="col-2 col-sm-2 col-md-2 col-lg-2 profile-image">
                 <img src={item.pic} class="image-height" />
               </div>
-                  <div class=" col-10 col-sm-10 col-md-10 col-lg-10">
+                  <div class=" col-8 col-sm-8 col-md-8 col-lg-8">
                     <div class="row"><div class="col-md-12"><h1 class="profile-name">{item.firstName} {item.lastName} </h1></div></div>
-                    <div class="row indent-row"><div class="col-md-12">Email {item.email} </div></div>
-                    <div class="row indent-row"><div class="col-md-12">Company {item.company} </div></div>
-                    <div class="row indent-row"><div class="col-md-12">Skill {item.skill} </div></div>
-                    <div class="row indent-row"><div class="col-md-12">Average {item.grades.reduce((sum, curr) => sum + Number(curr), 0) / item.grades.length}%</div></div>
+                    <div class="row indent-row"><div class="col-md-12">Email: {item.email} </div></div>
+                    <div class="row indent-row"><div class="col-md-12">Company: {item.company} </div></div>
+                    <div class="row indent-row"><div class="col-md-12">Skill: {item.skill} </div></div>
+                    <div class="row indent-row"><div class="col-md-12">Average: {item.grades.reduce((sum, curr) => sum + Number(curr), 0) / item.grades.length}%</div></div>
+                    <div class="row indent-row"> 
+                        <div id={item.id} class="col-md-12 collapse">
+                                  <ul class="grades-css">
+                                    {item.grades.map(grade => 
+                                  
+                                        <li>Test {i=i+1} &nbsp;&nbsp;&nbsp;&nbsp;{grade}%</li>)}
+                                        <p hidden>{i=0}</p> 
+                                  </ul>
+                        </div>
+                    </div>
                   </div>
-              {/* </li> */}
+                  <div class="col-2 col-sm-2 col-md-2 col-lg-2">
+                    <div class="row">
+                      <div class="col-12 col-sm-12 col-md-12 col-lg-12"> 
+                          <button type="button" class="glyphicon glyphicon-plus plus-button" data-toggle="collapse" data-target={"#"+item.id}></button>
+                      </div>
+                    </div>
+                  </div>
+              
               
             </div>
             
